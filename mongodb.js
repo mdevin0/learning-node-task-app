b86853422409ba7bd288c259b345f83776561ece
Mateus Devino <mdevino.coding@gmail.com>
2020-04-27 13:21:08 -0300

    Mongo findOne and find 101

--- mongodb.js
+++ mongodb.js
@@ -1,7 +1,6 @@
-const mongodb = require('mongodb');
+const {MongoClient, ObjectID} = require('mongodb');
 
 
-const MongoClient = mongodb.MongoClient;
 // It seems using localhost instead of the loopback address causes some performance issues.
 const connectionURL = 'mongodb://127.0.0.1:27017';
 const database = 'task-manager';
@@ -16,9 +15,47 @@ MongoClient.connect(connectionURL, {useNewUrlParser: true}, (error, client) => {
     console.log('Successfully connected with database.');
     const db = client.db(database);
 
-    db.collection('users').insertOne({
-        name: "Someone",
-        age: 25
+    // db.collection('users').insertOne({
+    //     name: "Someone",
+    //     age: 25
+    // }, (error, result) => {
+    //     if(error){
+    //         return console.error(error);
+    //     }
+    //     console.log("User saved successfully.", res.ops);
+    // });
+
+    // db.collection('tasks').insertMany([
+    //     {
+    //         description: "Finish Node.js course",
+    //         completed: false
+    //     },
+    //     {
+    //         description: "Security 101",
+    //         completed: false
+    //     },
+    //     {
+    //         description: "Express 101",
+    //         completed: true
+    //     }
+    // ], (error, result) => {
+    //     if(error){
+    //         return console.error(error);
+    //     }
+
+    //     console.log("Tasks inserted successfully.", result.ops);
+    // });
+
+    db.collection('tasks').findOne({_id: ObjectID('5ea6f0a63c95492bb8ab438d')}, (error, task) => {
+        if(error){
+            return console.log(console.error);
+        }
+
+        console.log("Task found:", task);
     });
 
+    db.collection('tasks').find({completed: false}).toArray((error, tasks) => {
+        console.log(`${tasks.length} unfinished tasks found:`);
+        console.log(tasks.map((task) => task.description).join('\n'));
+    });
 });
